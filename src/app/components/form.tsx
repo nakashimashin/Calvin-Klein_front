cde9418afda740aebc0a2706a39617f288681502
nakashimashin <nakashimashin0517@gmail.com>
2023-07-15 08:56:29 +0900

    second commit

---
+++ src/app/components/form.tsx
@@ -0,0 +1,8 @@
+
+export default function Form() {
+    return(
+        <form action="#" method="post" className="h-full w-80">
+            <textarea name="" className="bg-gray-100 rounded w-full " ></textarea>
+        </form>
+    )
+}
