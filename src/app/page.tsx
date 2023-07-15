f17e0a5b1611b5b8f5fb9b54286a929e9a0f370a
nakashimashin <nakashimashin0517@gmail.com>
2023-07-15 12:59:12 +0900

    入力フォーム作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import Form from "./components/form"
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="text-[3vw]">🐶AniMorse code🐱</div>
-      <div>テスト</div>
+      <div className="text-[32px] md:text-[54px] mt-[50px]">🐶AniMorse code🐱</div>
       <Form />
     </div>
   )
