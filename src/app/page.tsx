9605cfe47b41a65f7fbd2a00f13af43640fb6d18
nakashimashin <nakashimashin0517@gmail.com>
2023-07-15 16:57:54 +0900

    日本語のみ、英語のみシステム

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Form from "./components/form"
+// import Log from "./components/log"
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="text-[32px] md:text-[54px] mt-[50px]">🐶AniMorse code🐱</div>
       <Form />
+      {/* <Log /> */}
     </div>
   )
 }
