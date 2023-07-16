21eceb5f2b2252ada126e0a6113c160a04ca9fe7
shotaro-ada <syouta2223505@gmail.com>
2023-07-16 04:37:02 +0000

    fix

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -38,7 +38,7 @@ export default function Form() {
     const [formState, setFormState] = useState<Form>({ mode: 'wabun', animal: 'cat' })
     const [isLoaging, setIsLoading] = useState<boolean>(false);
     const [isResOk, setIsResOk] = useState<boolean>(false);
-    const [resVal, setResVal] = useState<ResVal>({});
+    const [resVal, setResVal] = useState<ResVal>(undefined);
 
     const {
         register,
